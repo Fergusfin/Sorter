430066a45961eed1f98ecd4f5da3be11a22eadae
Ethan Coward <ethanjc@mac.com>
2016-03-26 16:13:53 -0700

    Umm

---
+++ main.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+var ptn = require('parse-torrent-name');
+
+console.log(fs.readdirSync("//10.0.1.32/Media/Downloads"));
+
+//var test = ptn("Frozen.2013.1080p.WEB-DL.H264-PublicHD");
+
+//console.log(test.title)
